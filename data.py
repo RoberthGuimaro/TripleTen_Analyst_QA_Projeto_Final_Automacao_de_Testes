672c0b0141142e82d9016d6c677d445424a51990
RoberthGuimaro <guimaroroberth@gmail.com>
2025-08-08 19:51:58 -0400

    Created "data.py" file to store constants. Added TestUrbanRoutes class and defined tests within the class in the "main.py" file.

--- data.py
+++ data.py
@@ -1,1 +1,8 @@
 # Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-d7da4137-41eb-430f-be78-98e56c2d7cf2.containerhub.tripleten-services.com?lng=pt'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st ST'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MASSAGE_FOR_DRIVER = 'Pare no bar de sucos'
